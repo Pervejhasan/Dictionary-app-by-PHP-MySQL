6293f1fffabc87e5e1eda71bce3ad22a7cb20c29
Pervejhasan <mdpervej12441@gmail.com>
2023-01-13 02:22:40 +0600

    database create

--- insert.php
+++ insert.php
@@ -4,23 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <title>Dictionary App By PHP MySQL</title>
+    <link rel="stylesheet" href="./sass/main.css">
 </head>
-<body>
-    <h2>Insert Word</h2>
-
-   <div class="container w-25 ">
+<body class="bg-background">
+    <h2 class="d-flex justify-content-center mt-5 fs-1 text-decoration-underline">Insert Word</h2>
+   <div class="container w-25 mt-5">
    <form action="insert.php" method="post">
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">word</label>
+    <label for="exampleInputEmail1" class="form-label fs-5 ">word</label>
     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="word" placeholder="Enter Word" required>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Meaning</label>
-    <input type="text" class="form-control" id="exampleInputPassword1" name="word-meaning">
+    <label for="exampleInputPassword1" class="form-label fs-5 ">Meaning</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" name="meaning" placeholder="Enter Meaning" required>
   </div>
-  <button type="submit" class="btn btn-primary">Add Word</button>
+  <input type="submit" class="btn btn-button fw-bold" name="insert" value="Add Word"/>
 </form>
 
    </div>
